f750a486ad0f11232fb7b3956808f927ff1cd14a
Marcin Babalski <marcin.babalski@gmail.com>
2020-03-15 17:19:50 +0100

    Refactored registration test #3 & #4

--- page_object_pattern/pages/registration.py
+++ page_object_pattern/pages/registration.py
@@ -1,10 +1,12 @@
+from selenium.webdriver.support.ui import Select
+
+
 class Registration:
     def __init__(self, driver):
         self.driver = driver
         self.signin_button_css = 'login'
         self.email_field_css = 'email_create'
         self.createAccount_btn_css = '#SubmitCreate'
-        self.accountform_header_css = 'form#create-account_form > h3'
         self.registration_form_css = 'form#account-creation_form div:nth-child(1) > h3'
         self.register_button_css = 'submitAccount'
         self.radio_button_title_css = 'uniform-id_gender1'
@@ -30,22 +32,15 @@ class Registration:
     def click_signin(self):
         self.driver.find_element_by_class_name(self.signin_button_css).click()
 
-    def check_label(self):
-        auth_label = self.driver.find_element_by_css_selector(self.accountform_header_css)
-        return auth_label.text
-
-    def enter_invalid_mail(self):
-        self.driver.find_element_by_name(self.email_field_css).send_keys("asd")
+    def create_account(self):
         self.driver.find_element_by_css_selector(self.createAccount_btn_css).click()
 
     def invalid_mail_error(self):
         element = self.driver.find_element_by_css_selector(self.invalidEmail_error_css)
         return element.text
 
-    def enter_email(self):
-        self.driver.find_element_by_class_name(self.signin_button_css).click()
-        self.driver.find_element_by_name(self.email_field_css).send_keys("asd2@asd.pl")
-        self.driver.find_element_by_css_selector(self.createAccount_btn_css).click()
+    def enter_email(self, email):
+        self.driver.find_element_by_name(self.email_field_css).send_keys(email)
 
     def register_account(self):
         self.driver.find_element_by_id(self.register_button_css).click()
@@ -57,3 +52,27 @@ class Registration:
     def registration_error(self):
         error = self.driver.find_element_by_css_selector(self.registration_error_css)
         return error.text
+
+    def exisiting_account_error(self):
+        error = self.driver.find_element_by_css_selector(self.createAccount_error_css)
+        return error.text
+
+    def populate_registration_form(self):
+        self.driver.find_element_by_name(self.email_field_css).send_keys('mobiy43412@cityroyal.org')
+        self.driver.find_element_by_css_selector(self.createAccount_btn_css).click()
+        self.driver.find_element_by_id(self.radio_button_title_css).click()
+        self.driver.find_element_by_name(self.input_customer_firstname_css).send_keys("John")
+        self.driver.find_element_by_name(self.input_customer_lastname_css).send_keys("Doe")
+        self.driver.find_element_by_name(self.input_password_css).send_keys("XDWZP8123")
+        self.driver.find_element_by_name(self.input_firstname_css).send_keys("John")
+        self.driver.find_element_by_name(self.input_lastname_css).send_keys("Doe")
+        self.driver.find_element_by_name(self.input_address_css).send_keys("Westeria Lane")
+        self.driver.find_element_by_name(self.input_city_css).send_keys("Denver")
+        dropdown = Select(self.driver.find_element_by_id(self.dropdown_state_css))
+        dropdown.select_by_index(6)
+        self.driver.find_element_by_name(self.input_zipcode_css).send_keys("90210")
+        self.driver.find_element_by_name(self.input_mobile_css).send_keys("8588489004")
+
+    def account_landing_page(self):
+        my_account = self.driver.find_element_by_css_selector(self.account_page_css)
+        assert my_account.text
--- page_object_pattern/tests/test_registration.py
+++ page_object_pattern/tests/test_registration.py
@@ -8,46 +8,36 @@ from assertpy import assert_that
 @allure.description("Tests validating proper handling of registration feature")
 class TestRegistration:
 
-    @pytest.mark.usefixtures('setup')
+    @allure.title('Register account - negative (invalid email address)')
     def test_sing_in_negative(self, setup):
         self.registration.click_signin()
-        assert_that(self.registration.check_label()).contains('CREATE AN ACCOUNT')
-        self.registration.enter_invalid_mail()
+        self.registration.enter_email('asd')
+        self.registration.create_account()
         assert_that(self.registration.invalid_mail_error()).contains("Invalid email address.")
 
-    @pytest.mark.usefixtures('setup')
+    @allure.title('Register account - negative (mandatory fields not populated)')
     def test_sing_in_negative2(self, setup):
-        self.registration.enter_email()
-        assert_that(self.registration.registration_screen()).contains('YOUR PERSONAL INFORMATION')
+        self.registration.click_signin()
+        self.registration.enter_email('asd2@asd.pl')
+        self.registration.create_account()
         self.registration.register_account()
         assert_that(self.registration.registration_error()).contains('There are 8 errors')
 
-    @pytest.mark.usefixtures('setup')
-    def test_sing_in_negative3():
-        self.driver.find_element_by_class_name(signin_button()).click()
-        self.driver.find_element_by_name(email_field()).send_keys("mobiy43402@cityroyal.org")
-        self.driver.find_element_by_css_selector(createAccount_button()).click()
-        error = self.driver.find_element_by_css_selector(createAccount_error())
-        assert error.text == 'An account using this email address has already been registered. Please enter a valid password or request a new one.'
+    @allure.title('Register account - negative (existing account)')
+    def test_sing_in_negative3(self, setup):
+        self.registration.click_signin()
+        self.registration.enter_email('mobiy43402@cityroyal.org')
+        self.registration.create_account()
+        assert_that(self.registration.exisiting_account_error()).contains('An account using this email address has '
+                                                                          'already been registered. Please enter a '
+                                                                          'valid password or request a new one.')
 
-    @pytest.mark.usefixtures('setup')
-    def test_sing_in_positive():
-        self.driver.find_element_by_class_name(signin_button()).click()
-        self.driver.find_element_by_name(email_field()).send_keys(dg.generate_email(ptf=False))
-        self.driver.find_element_by_css_selector(createAccount_button()).click()
-        self.driver.find_element_by_id(radio_button_title()).click()
-        self.driver.find_element_by_name(input_customer_firstname()).send_keys("John")
-        self.driver.find_element_by_name(input_customer_lastname()).send_keys("Doe")
-        self.driver.find_element_by_name(input_password()).send_keys("XDWZP8123")
-        self.driver.find_element_by_name(input_firstname()).send_keys("John")
-        self.driver.find_element_by_name(input_lastname()).send_keys("Doe")
-        self.driver.find_element_by_name(input_address()).send_keys("Westeria Lane")
-        self.driver.find_element_by_name(input_city()).send_keys("Denver")
-        dropdown = Select(self.driver.find_element_by_id(dropdown_state()))
-        dropdown.select_by_index(6)
-        self.driver.find_element_by_name(input_zipcode()).send_keys("90210")
-        self.driver.find_element_by_name(input_mobile()).send_keys("8588489004")
-        self.driver.find_element_by_id(register_button()).click()
-        my_account = self.driver.find_element_by_css_selector(account_page())
-        assert my_account.text == 'MY ACCOUNT'
+    @allure.title('Register account - positive')
+    def test_sing_in_positive(self, setup):
+        self.registration.click_signin()
+        self.registration.enter_email('mobiy43452@cityroyal.org')
+        self.registration.create_account()
+        self.registration.populate_registration_form()
+        self.registration.register_account()
+        assert_that(self.registration.account_landing_page()).contains('MY ACCOUNT')
 
