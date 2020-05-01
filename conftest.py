dc06f6cc194e959fac3ec04d86d85fbcfd7e7f0b
Marcin Babalski <marcin.babalski@gmail.com>
2020-05-01 16:05:39 +0200

    Updated conftest

--- conftest.py
+++ conftest.py
@@ -1,10 +1,13 @@
 import pytest
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from page_object_pattern.pages.login_page import Login
 from page_object_pattern.pages.shopping_cart_page import ShoppingCart
 from page_object_pattern.pages.registration_page import Registration
+
+HUB_SERVER = os.getenv('HUB_SERVER')
 
 
 @pytest.fixture()
@@ -13,7 +16,7 @@ def setup(request):
     options.set_capability('browseName', 'Chrome')
     options.add_argument('headless')
     options.add_argument('start-maximized')
-    driver = webdriver.Remote('192.168.1.91:4444/wd/hub', options=options)
+    driver = webdriver.Remote(HUB_SERVER, options=options)
     driver.get('http://automationpractice.com/index.php')
     driver.maximize_window()
     driver.implicitly_wait(10)
